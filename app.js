0c006e0aa29a1e741ec3d9ae1b34c194ca90ff8f
jake-sh <cibatool@gmail.com>
2026-04-21 17:21:44 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -484,7 +484,11 @@ function renderFriendList() {
     <div class="friend-item">
       <div class="friend-avatar" onclick="openChat('${f}')">${f[0]}</div>
       <div class="friend-name" onclick="openChat('${f}')">${f}</div>
-      <button class="friend-del-btn" onclick="deleteChat('${f}')">🗑</button>
+      <button class="friend-del-btn" onclick="deleteChat('${f}')">
+        <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round">
+          <polyline points="3 6 5 6 21 6"/><path d="M19 6l-1 14H6L5 6"/><path d="M10 11v6"/><path d="M14 11v6"/><path d="M9 6V4h6v2"/>
+        </svg>
+      </button>
     </div>`).join('');
 }
 
