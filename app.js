05857f939bbfef5ac7149ddf79caf62f126571c0
jake-sh <cibatool@gmail.com>
2026-05-12 17:19:45 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -927,7 +927,13 @@ function enterChatApp() {
     document.getElementById('friendListView').style.display = 'none';
     document.getElementById('activeChatView').style.display = 'none';
   } else {
-    showFriendList();
+    var f = JSON.parse(localStorage.getItem('friends') || '[]');
+    if (f.length === 1) {
+      listenFriendChanges();
+      openChat(f[0]);
+    } else {
+      showFriendList();
+    }
   }
 }
 
