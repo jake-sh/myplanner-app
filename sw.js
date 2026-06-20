38210b39595511b8e2f9e769fa58d4a408502a9f
jake-sh <cibatool@gmail.com>
2026-06-20 21:34:26 +0900

    버전 v3.9.0로 정정 (v3.8.10 표기 오류 수정)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'myplanner-v3810';
+const CACHE = 'myplanner-v390';
 const PRECACHE = ['./', './index.html', './app.js', './style.css', './manifest.json'];
 
 self.addEventListener('install', e => {
