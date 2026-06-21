4289b5dd7de828bb62275a8b81883c0f8e437ea0
jake-sh <cibatool@gmail.com>
2026-06-21 15:59:50 +0900

    v4.0.2: 9개 메뉴 아이콘 카드 디자인을 날씨 위젯과 동일하게 통일 (그림자+테두리)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'myplanner-v401';
+const CACHE = 'myplanner-v402';
 const PRECACHE = ['./', './index.html', './app.js', './style.css', './manifest.json'];
 
 self.addEventListener('install', e => {
