0c9df0f22b6d4e210189ec0609dbb0a869ab76e2
jake-sh <cibatool@gmail.com>
2026-06-21 15:57:38 +0900

    v4.0.1: 할일 화면 입력창과 목록 카드의 좌우 폭 통일

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'myplanner-v400';
+const CACHE = 'myplanner-v401';
 const PRECACHE = ['./', './index.html', './app.js', './style.css', './manifest.json'];
 
 self.addEventListener('install', e => {
