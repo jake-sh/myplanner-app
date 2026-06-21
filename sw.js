20e0db8c41443df57f50f7581c0fe4e95b7ef2f0
jake-sh <cibatool@gmail.com>
2026-06-21 15:11:36 +0900

    v3.9.3: 메인화면 타이틀-날짜/시계 영역 사이 구분선 제거

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'myplanner-v392';
+const CACHE = 'myplanner-v393';
 const PRECACHE = ['./', './index.html', './app.js', './style.css', './manifest.json'];
 
 self.addEventListener('install', e => {
