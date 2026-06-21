04359ddc5f3f2006e0598a84fd171c9afc8e74b1
jake-sh <cibatool@gmail.com>
2026-06-21 22:53:13 +0900

    v4.0.9: icon-badge.png 교체

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'myplanner-v408';
+const CACHE = 'myplanner-v409';
 const PRECACHE = ['./', './index.html', './app.js', './style.css', './manifest.json'];
 
 self.addEventListener('install', e => {
