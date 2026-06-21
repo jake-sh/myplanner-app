37d4224acb58639becbcdf67f6f1a44ad9a60dcb
jake-sh <cibatool@gmail.com>
2026-06-21 19:18:40 +0900

    v4.0.5: 메뉴 카드 border가 안 보이던 원인(예전 nth-child(n) 규칙) 제거
    이전 "구분선 제거" 작업 때 추가한 .menu-item:nth-child(n){border:none;}
    규칙이 specificity가 더 높아서 v4.0.2/4.0.4에서 다시 추가한 border를
    계속 무효화하고 있었음. 해당 규칙 삭제로 border 정상 표시.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'myplanner-v404';
+const CACHE = 'myplanner-v405';
 const PRECACHE = ['./', './index.html', './app.js', './style.css', './manifest.json'];
 
 self.addEventListener('install', e => {
