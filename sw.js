015c1c45d519a159a9dcdcd09913738cbbdd7578
jake-sh <cibatool@gmail.com>
2026-06-21 15:28:25 +0900

    v3.9.5: 사용자 제작 알림 배지 아이콘으로 교체

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'myplanner-v394';
+const CACHE = 'myplanner-v395';
 const PRECACHE = ['./', './index.html', './app.js', './style.css', './manifest.json'];
 
 self.addEventListener('install', e => {
