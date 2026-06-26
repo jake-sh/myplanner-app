86f6f6ec6901f0433dadd854c6d79b7867ee8b0b
jake-sh <cibatool@gmail.com>
2026-06-27 01:31:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'myplanner-v413';
+const CACHE = 'myplanner-v414';
 const PRECACHE = ['./', './index.html', './app.js', './style.css', './manifest.json'];
 
 self.addEventListener('install', e => {
